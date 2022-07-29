6e188ca77b99e944673eb64799d7cd8f57f175c5
Iury1978 <advent777@yandex.ru>
2022-07-29 11:01:58 +0300

    created block remark. html, css and js.

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ import "./module/menu.js";
 import "./module/modal.js";
 import "./module/about-us-tabs.js";
 import "./module/exchange-rates.js";
+import "./module/remark.js";
 import { accodrion } from "./module/faq.js";
 
 accodrion();
---
+++ js/module/remark.js
@@ -0,0 +1,16 @@
+const remark = document.querySelector(".remark");
+let height = remark.offsetHeight;
+remark.style.height = `${height}px`;
+
+const closeRemark = () => {
+  height -= 4;
+  remark.style.height = `${height}px`;
+  if (height > 0) {
+    requestAnimationFrame(closeRemark);
+  } else {
+    remark.style.height = 0;
+    remark.style.display = "none";
+  }
+};
+
+remark.addEventListener("click", closeRemark);
